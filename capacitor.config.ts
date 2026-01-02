33c96305a0011c1bc9265419615b4cdf5ac169f8
tishbian-meshach <www.stishbian262@gmail.com>
2026-01-02 20:17:06 +0530

    updated capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.p2pwatchparty',
   appName: 'StreamWing',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://stream-wing.vercel.app/',
+    cleartext: true
+  }
 };
 
 export default config;
